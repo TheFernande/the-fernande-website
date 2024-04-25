4d8ca87e73a895a274fa34b2482bc8a67f9d6fc9
Jade Hernandez <jade.hernandez88@gmail.com>
2024-04-25 17:37:59 +0200

    feat(the-fernande): :technologist: DX and IDE setup
    Added Husky pre-commit hook. Added stylelint config. Added styled-components config. Added bundling config.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true"
+});
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  },
+  swcMinify: true,
+  trailingSlash: true,
+  // output: "standalone",
+  env: {}
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
